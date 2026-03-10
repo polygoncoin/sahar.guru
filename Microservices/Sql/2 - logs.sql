0a4f9240ec295d14b652b011963e02e7f827e303
“polygoncoin <polygon.co.in@gmail.com>
2026-03-10 18:10:11 +0530

    Converted Indentation to Tab

--- Microservices/Sql/2 - logs.sql
+++ Microservices/Sql/2 - logs.sql
@@ -5,7 +5,7 @@ CREATE TABLE `request` (
     `request_by` ENUM('Admin', 'Customer', 'WebsiteAdmin') NOT NULL,
     `request_by_no` BIGINT NOT NULL,
     `request_route` VARCHAR(250),
-    `request_method` ENUM('POST', 'PUT', 'PATCH', 'DELETE') NOT NULL,
+    `request_method` ENUM('GET', 'POST', 'PUT', 'PATCH', 'DELETE') NOT NULL,
     `request_payload_json` JSON NOT NULL,
     `request_operated_on` DATETIME DEFAULT CURRENT_TIMESTAMP,
     `request_operator_ip` VARCHAR(25) NOT NULL,
@@ -19,7 +19,7 @@ CREATE TABLE `error_log` (
     `operated_by` ENUM('Admin', 'Customer', 'WebsiteAdmin') NOT NULL,
     `operated_by_no` BIGINT NOT NULL,
     `operated_route` VARCHAR(250),
-    `operated_request_method` ENUM('POST', 'PUT', 'PATCH', 'DELETE') NOT NULL,
+    `operated_request_method` ENUM('GET', 'POST', 'PUT', 'PATCH', 'DELETE') NOT NULL,
     `operated_config_json` JSON NOT NULL,
     `operated_payload_json` JSON NOT NULL,
     `operated_session_json` JSON NOT NULL,
@@ -27,5 +27,23 @@ CREATE TABLE `error_log` (
     `operated_on` DATETIME DEFAULT CURRENT_TIMESTAMP,
     `operator_ip` VARCHAR(25) NOT NULL,
     PRIMARY KEY (`error_log_no`)
+) ENGINE = InnoDB;
+
+DROP TABLE IF EXISTS `debug_log`;
+CREATE TABLE `debug_log` (
+    `debug_log_no` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `debug_mode` VARCHAR(250),
+    `request_no` BIGINT NOT NULL,
+    `operated_by` ENUM('Admin', 'Customer', 'WebsiteAdmin') NOT NULL,
+    `operated_by_no` BIGINT NOT NULL,
+    `operated_route` VARCHAR(250),
+    `operated_request_method` ENUM('GET', 'POST', 'PUT', 'PATCH', 'DELETE') NOT NULL,
+    `operated_config_json` JSON NOT NULL,
+    `operated_payload_json` JSON NOT NULL,
+    `operated_session_json` JSON NOT NULL,
+    `exception_json` JSON NOT NULL,
+    `operated_on` DATETIME DEFAULT CURRENT_TIMESTAMP,
+    `operator_ip` VARCHAR(25) NOT NULL,
+    PRIMARY KEY (`debug_log_no`)
 ) ENGINE = InnoDB;
 -- ----------- Tables for logging --------------
